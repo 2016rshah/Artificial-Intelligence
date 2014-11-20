b3d85bb64b56af25b08bf4add579c5861fb85bdf
Rushi Shah <2016rshah@gmail.com>
2014-11-19 19:47:56 -0500

    It works! Other than the occasional infinite loop..

--- sudoku/sudoku2.py
+++ sudoku/sudoku2.py
@@ -35,15 +35,15 @@ COLS = 9
 # 	[6,9,5,4,1,7,3,8,2]
 # ]
 M = [
-	[0,8,0,9,2,1,6,5,7],
-	[0,0,0,3,4,5,8,2,1],
-	[2,5,0,0,7,6,4,9,3],
-	[5,4,8,0,3,2,9,7,6],
-	[7,2,9,0,6,4,1,3,8],
-	[1,3,6,7,9,8,2,4,5],
-	[3,7,2,6,8,9,5,1,4],
-	[8,1,4,2,5,3,7,6,9],
-	[6,9,5,4,1,7,3,8,2]
+	[0,0,0,9,2,1,6,5,7],
+	[0,0,0,3,4,5,0,2,0],
+	[0,5,0,0,7,6,0,0,0],
+	[5,0,8,0,3,2,0,7,6],
+	[7,0,9,0,0,0,1,3,8],
+	[0,0,6,7,0,0,2,0,0],
+	[3,7,2,0,8,9,0,1,4],
+	[8,1,4,2,5,0,7,6,0],
+	[6,9,5,4,1,7,3,0,0]
 ]
 class Cell(object):
 	matrix = None
@@ -79,8 +79,9 @@ class Cell(object):
 			elif(c<=8):
 				return 8
 	def __repr__(self):
-		if(self.value == {1, 2, 3, 4, 5, 6, 7, 8, 9}): return str({0})
-		else: return str(self.value)
+		# if(self.value == {1, 2, 3, 4, 5, 6, 7, 8, 9}): return str({0})
+		# else: return str(self.value)
+		return str(self.value)
 def printMatrix(m):
 	print("[")
 	for row in range(len(m)):
@@ -141,7 +142,6 @@ def fillRows(M):
 				emptyIndices.append(j)
 		if(len(needed) == 1 and len(emptyIndices) == 1):
 			m[i][emptyIndices[0]] = needed[0]
-			print("replaced..")
 		elif(len(needed)>1 and len(needed)<len(lowestNeeded)):
 			lowestNeeded = needed
 	if(m!=M):
@@ -163,7 +163,6 @@ def fillCols(M):
 				emptyIndices.append(row)
 		if(len(needed) == 1 and len(emptyIndices) == 1):
 			m[emptyIndices[0]][col] = needed[0]
-			print("replaced..")
 		elif(len(needed)>1 and len(needed)<len(lowestNeeded)):
 			lowestNeeded = deepcopy(needed)
 	if(m!=M):
@@ -190,7 +189,6 @@ def fillBoxes(M):
 						emptyIndices.append([i, j])
 			if(len(needed) == 1 and len(emptyIndices) == 1):
 				m[emptyIndices[0][0]][emptyIndices[0][1]] = needed[0]
-				print("replaced..")
 			elif(len(needed)>1 and len(needed)<len(lowestNeeded)):
 				lowestNeeded = deepcopy(needed)
 	if(m!=M):
@@ -223,22 +221,116 @@ def createMatrixObject(M):
 			row.append(Cell(M[r][c], r, c, matrix))
 		matrix.append(row)
 	return matrix
-def trickTwo(MO):
-	#once you figure out trick two, put it here
+def candidates(MO):
+	mo = deepcopy(MO)
+	for row in range(len(mo)):
+		for col in range(len(mo[row])):
+			candidates = mo[row][col].value
+			if(len(candidates)>1):
+				remSet = set()
+				for i in range(len(mo)):
+					if(len(mo[i][col].value) == 1):
+						temp = mo[i][col].value.pop()
+						mo[i][col].value.add(temp)
+						remSet.add(temp)
+				for j in range(len(mo)):
+					if(len(mo[row][j].value) == 1):
+						temp = mo[row][j].value.pop()
+						mo[row][j].value.add(temp)
+						remSet.add(temp)
+				i = 0; j = 0;
+				for i in range(len(mo)):
+					for j in range(len(mo)):
+						if(len(mo[i][j].value) == 1 and mo[i][j].block == mo[row][col].block):
+							temp = mo[i][j].value.pop()
+							mo[i][j].value.add(temp)
+							remSet.add(temp)
+				candidates = candidates - remSet
+				mo[row][col].value = candidates
+	return mo
+
+def printMatrixObject(MO):
+	for row in range(len(MO)):
+		s = ""
+		for col in range(len(MO[row])):
+			s+=str((MO[row][col].value))
+		print(s)
+def convertMOtoM(MO):
+	mo = deepcopy(MO)
+	res = []
+	for i in range(len(mo)):
+		res.append([])
+		for j in range(len(mo[i])):
+			res[i].append(0)
+			if(len(mo[i][j].value) == 1):
+				res[i][j] = mo[i][j].value.pop()
+	return res
+def coordinatesOfCellWithSmallestValueSet(MO):
+	mo = deepcopy(MO)
+	smallest = 20
+	coords = [-1, -1]
+	for i in range(len(mo)):
+		for j in range(len(mo[i])):
+			if(len(mo[i][j].value) > 1 and len(mo[i][j].value) < smallest):
+				smallest = len(mo[i][j].value)
+				coords = [i, j]
+	if(coords[0]!=-1):
+		return coords
+def revert(MO, oldMO):
+	for r in range(len(MO)):
+		for c in range(len(MO[r])):
+			MO[r][c].value = oldMO[r][c].value
 	return MO
 def recursivelySolve(M):
+	if(verifyTable(M)):
+		return M
+	#trick 1
 	m = deepcopy(M)
 	m = makeSimpleChanges(m)
+	if(verifyTable(m)):
+		print("Trick 1 successfull")
+		return m
+	else:
+		#print("Trick 1 failed, trying trick two:")
+		pass
 
-	mo = createMatrixObject(M)
-	mo = trickTwo(mo)
+	#trick 2
+	mo = createMatrixObject(m)
+	mo = candidates(mo)
+	m = convertMOtoM(mo)
+	if(verifyTable(m)):
+		print("Trick 2 successfull")
+		return m
+	else:
+		#print("Trick 2 failed, trying trick three:")
+		pass
 
-
+	#trick 3
+	if(not verifyTable(m)):
+		mo = createMatrixObject(m)
+		oldmo = deepcopy(mo)
+		coords = coordinatesOfCellWithSmallestValueSet(mo) #This starts getting stuck in an infinite loop between [8, 4] and [8, 5]
+		if(coords):
+			r = coords[0]
+			c = coords[1]
+			for guess in mo[r][c].value:
+				print("Making guess at: ", coords)
+				print("Made guess: ", guess)
+				mo[r][c].value = {guess,}
+				tempM = convertMOtoM(mo)
+				tempM = recursivelySolve(tempM)
+				if(verifyTable(tempM)):
+					print("Guess successful!")
+					return tempM
+				else:
+					mo = revert(mo, oldmo)
+					print("Nope! Reverting and possibly trying again")
 	return m
+
+print(verifyTable(M))
 printMatrix(M)
 print("------Working-----")
 M = recursivelySolve(M)
 print("-----------------")
 print(verifyTable(M))
 printMatrix(M)
-# print(M)
