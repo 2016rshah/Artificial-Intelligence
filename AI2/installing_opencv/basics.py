9bf1ac672e5540940e65cb2e95b0b909cb374778
Rushi Shah <2016rshah@gmail.com>
2016-02-18 13:45:59 -0500

    Guess I'll just have to run with Python 2

--- AI2/installing_opencv/basics.py
+++ AI2/installing_opencv/basics.py
@@ -1,5 +1,12 @@
 import cv2
 
-img = cv2.imread("prof-pic2.jpg")
-cv2.imshow('image', img)
-input("okay?")
+def main():
+    filename = input("What is the filename for the image")
+    if filename.length == 0:
+        filename = "prof-pic2.jpg"
+    img = cv2.imread(filename, 0)
+    cv2.imshow('image', img)
+    #k = cv2.waitKey(0)
+    #if k == 27:
+    #    cv2.destroyAllWindows()
+    
