9c7bb69ac323e256fe4f6e0b5bb98c61bfe5e9c8
Rushi Shah <2016rshah@gmail.com>
2016-02-22 17:47:05 -0500

    Finished canny edge detection

--- AI2/computer_vision/script.py
+++ AI2/computer_vision/script.py
@@ -99,41 +99,14 @@ def getGvalue(img, i, j):
 
 
 def sobelPixel(img, i, j, threshold):
-	# gx = 0
-	# gy = 0 
-	# # gx = [
-	# # [-1, 0, 1],
-	# # [-2, 0, 2],
-	# # [-1, 0, 1]]
-	# gx += (-1) * img.item(i-1, j-1, 0) #top left
-	# gx += img.item(i+1, j-1, 0) # top right
-	# gx += (-2) * img.item(i-1, j, 0) # middle left
-	# gx += 2 * img.item(i+1, j, 0) # middle right
-	# gx += (-1) * img.item(i-1, j+1, 0) # bottom left
-	# gx += img.item(i+1, j+1, 0) # bottom right
-
-	# # gy = [
-	# # [-1, -2, -1],
-	# # [0, 0, 0],
-	# # [1, 2, 1]]
-	# gy += (-1) * img.item(i-1, j-1, 0) #top left
-	# gy += (-2) * img.item(i-1, j, 0) # top middle
-	# gy += (-1) * img.item(i+1, j-1, 0) # top right
-	# gy += img.item(i-1, j+1, 0) # bottom right
-	# gy += (2) * img.item(i, j+1, 0) # bottom middle
-	# gy += img.item(i+1, j+1, 0) # bottom right
-
-	# gSquared = gx * gx + gy * gy
-	# # print(gx, gy)
-
 	derivs = getDerivatives(img, i, j)
 	gx = derivs[0]
 	gy = derivs[1]
 
 	if(((gx * gx) + (gy * gy)) > (threshold * threshold)):
-		return (255, 255, 255) # black pixel
+		return (0,0,0) # white pixel
 	else:
-		return (0, 0, 0) # white pixel
+		return (255, 255, 255) # black pixel
 
 def sobelImage(image):
 	img = image.copy()
@@ -141,17 +114,39 @@ def sobelImage(image):
 	num_cols = img.shape[1] - 2
 	for i in range(1, num_rows):
 		for j in range(1, num_cols):
-			setPixel(img, i, j, sobelPixel(image, i, j, MID_THRESHOLD)) #image is not changed, img is changed
+			setPixel(img, i, j, sobelPixel(image, i, j, MID_THRESHOLD)) 
+			#image is not changed, img is changed
 	return img
 
-def cannyPixel(image, i, j, lower_t, upper_t):
-	derivs = getDerivatives(img, j, j)
+def neighbors(img, i, j, di, dj):
+	low = getGvalue(img, i-di, j-di)
+	high = getGvalue(img, i+di, j+dj)
+	# if(low > )
+
+def isEdge(img, i, j, lower_t, upper_t):
+	if(i<1 or j<1 or i>=img.shape[0] or j>=img.shape[1]):
+		return False
+
+	derivs = getDerivatives(img, i, j)
 	gx = derivs[0]
 	gy = derivs[1]
+	g = gx * gx + gy * gy
 
-	theta = atan2(gy, gx)
+	theta = atan2(gy, gx) # I think this goes from negative pi to pi
 	
-	getGvalue(img, i+1, j+1)
+	# print(theta)
+	if(g > upper_t * upper_t):
+		return False
+	elif(g < lower_t * lower_t):
+		return True
+	else:
+		return isEdge(img, i-1, j-1, lower_t, upper_t) or isEdge(img, i-1, j-1, lower_t, upper_t) 
+		#this will fail if it recurs out of bounds
+	# elif(g >= getGvalue(img, i-1, j) and g >= getGvalue(img, i+1, j)):
+	# 	return (0,255,0)
+	# else:
+
+
 
 def cannyImage(image):
 	img = image.copy()
@@ -159,7 +154,10 @@ def cannyImage(image):
 	num_cols = img.shape[1] - 4
 	for i in range(2, num_rows):
 		for j in range(2, num_cols):
-			setPixel(img, i, j, cannyPixel(image, i, j, MID_THRESHOLD-50, MID_THRESHOLD+50)) #image is not changed, img is changed
+			if(isEdge(image, i, j, MID_THRESHOLD-DT, MID_THRESHOLD+DT)): #image is not changed, img is changed
+				setPixel(img, i, j, (255, 255, 255)) 
+			else:
+				setPixel(img, i, j, (0, 0, 0))
 	return img
 
 def keystroke():
@@ -184,10 +182,10 @@ def keystroke():
 		cv2.destroyAllWindows()
 		cv2.imshow('sobel_image',sobel_image)
 		print('showing sobel image')
-	# elif k == ord('c'):
-	# 	cv2.destroyAllWindows()
-	# 	cv2.imshow('canny_image',canny_image)
-	# 	print('showing canny image')
+	elif k == ord('c'):
+		cv2.destroyAllWindows()
+		cv2.imshow('canny_image',canny_image)
+		print('showing canny image')
 
 	print("recursing")
 	keystroke()
@@ -195,6 +193,7 @@ def keystroke():
 location = sys.argv[1]
 
 MID_THRESHOLD = int(sys.argv[2])
+DT = 25
 
 #if I want to read from URL, do that here. Until them, I treat it as a local url 
 image = cv2.imread(location)
@@ -209,8 +208,8 @@ cv2.imshow('blur_image',blur_image)
 sobel_image = sobelImage(gray_image)
 cv2.imshow('sobel_image',sobel_image)
 
-# canny_image = cannyImage(gray_image)
-# cv2.imshow('canny_image', canny_image)
+canny_image = cannyImage(gray_image)
+cv2.imshow('canny_image', canny_image)
 
 keystroke()
 
