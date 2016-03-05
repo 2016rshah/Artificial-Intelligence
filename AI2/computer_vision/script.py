c15349df7538d3e56d602744b4e1cc98baabfdf7
Rushi Shah <2016rshah@gmail.com>
2016-03-05 12:03:30 -0500

    Finished canny2 for real this time

--- AI2/computer_vision/script.py
+++ AI2/computer_vision/script.py
@@ -1,9 +1,48 @@
 import cv2
 import numpy as np
+import sys
 from math import atan2
 from math import pi
-import sys
 
+LOCATION = sys.argv[1]
+THRESHOLD = int(sys.argv[2])
+BLACK_PIXEL = (0, 0, 0)
+WHITE_PIXEL = (255,255,255)
+RED_PIXEL = (255, 0, 0)
+
+def keystroke():
+	#Escape key to exit
+	k = cv2.waitKey(0)
+	if k == 27:         # wait for ESC key to exit
+	    cv2.destroyAllWindows()
+	    exit()
+	elif k == ord('n'):
+		cv2.destroyAllWindows()
+		cv2.imshow('color_image',image)
+		print('showing normal image')
+	elif k == ord('g'):
+		cv2.destroyAllWindows()
+		cv2.imshow('gray_image', gray_image)
+		print('showing gray image')
+	elif k == ord('b'):
+		cv2.destroyAllWindows()
+		cv2.imshow('blur_image', blur_image)
+		print('showing blur image')
+	elif k == ord('s'):
+		cv2.destroyAllWindows()
+		cv2.imshow('sobel_image', sobel_image)
+		print('showing sobel image')
+	elif k == ord('1'):
+		cv2.destroyAllWindows()
+		cv2.imshow('canny1_image', canny1_image)
+		print('showing canny1 image')
+	elif k == ord('2'):
+		cv2.destroyAllWindows()
+		cv2.imshow('canny2_image', canny2_image)
+		print('showing canny2 image')
+
+	print("recursing")
+	keystroke()
 
 def setPixel(img, row, col, newPixel):
 	'''
@@ -16,10 +55,24 @@ def setPixel(img, row, col, newPixel):
 	img.itemset((row, col, 1), newPixel[1])
 	img.itemset((row, col, 2), newPixel[2])
 
-
+def getPixel(img, row, col):
+	return (img.item(row, col, 0), img.item(row, col, 1), img.item(row, col, 2))
+	
 def grayPixel(r, g, b):
 	pxVal = .3 * r + .59 * g +  .11 * b
 	return (pxVal, pxVal, pxVal)
+
+def grayImage(image):
+	img = image.copy()
+	num_rows = img.shape[0]
+	num_cols = img.shape[1]
+	for i in range(num_rows):
+		for j in range(num_cols):
+			r = img.item(i, j, 0)
+			g = img.item(i, j, 1)
+			b = img.item(i, j, 2)
+			setPixel(img, i, j, grayPixel(r, g, b)) 
+	return img
 
 def blurPixel(img, row, col):
 	vals = []
@@ -39,20 +92,6 @@ def blurPixel(img, row, col):
 
 	return (val, val, val)
 
-def grayImage(image):
-	'''returns grayed image'''
-	img = image.copy()
-	num_rows = img.shape[0]
-	num_cols = img.shape[1]
-
-	for i in range(num_rows):
-		for j in range(num_cols):
-			r = img.item(i, j, 0)
-			g = img.item(i, j, 1)
-			b = img.item(i, j, 2)
-			setPixel(img, i, j, grayPixel(r, g, b)) 
-	return img
-
 def blurImage(image):
 	'''returns blurred image'''
 	img = image.copy()
@@ -65,6 +104,7 @@ def blurImage(image):
 	return img 
 
 def getDerivatives(img, i, j):
+	'''Returns a tuple with gx and gy'''
 	gx = 0
 	gy = 0 
 	# gx = [
@@ -91,23 +131,15 @@ def getDerivatives(img, i, j):
 
 	return (gx, gy)
 
-def getGvalue(img, i, j):
-	derivs = getDerivatives(img, j, j)
+def sobelPixel(img, row, col):
+	derivs = getDerivatives(img, row, col)
 	gx = derivs[0]
 	gy = derivs[1]
-
-	return gx * gx + gy * gy
-
-
-def sobelPixel(img, i, j, threshold):
-	derivs = getDerivatives(img, i, j)
-	gx = derivs[0]
-	gy = derivs[1]
-
-	if(((gx * gx) + (gy * gy)) > (threshold * threshold)):
-		return (0,0,0) # black pixel
+	g = gx * gx + gy * gy
+	if(g > THRESHOLD * THRESHOLD):
+		return BLACK_PIXEL #edge
 	else:
-		return (255, 255, 255) # white pixel
+		return WHITE_PIXEL
 
 def sobelImage(image):
 	img = image.copy()
@@ -115,239 +147,134 @@ def sobelImage(image):
 	num_cols = img.shape[1] - 2
 	for i in range(1, num_rows):
 		for j in range(1, num_cols):
-			setPixel(img, i, j, sobelPixel(image, i, j, MID_THRESHOLD)) 
+			setPixel(img, i, j, sobelPixel(image, i, j)) 
 			#image is not changed, img is changed
 	return img
 
-# def neighbors(img, i, j, di, dj):
-# 	low = getGvalue(img, i-di, j-di)
-# 	high = getGvalue(img, i+di, j+dj)
-# 	# if(low > )
-
-def isEdge(img, i, j, lower_t, upper_t):
-	if(i<1 or j<1 or i>=img.shape[0] or j>=img.shape[1]):
-		return False
-
-	derivs = getDerivatives(img, i, j)
-	gx = derivs[0]
-	gy = derivs[1]
-	g = gx * gx + gy * gy
-	#print(g, lower_t * lower_t, upper_t * upper_t)
-	#print(g)
-
-	if(g > upper_t * upper_t):
-		return False
-	elif(g < lower_t * lower_t):
-		return True
-	else:
-		return True
-
-def isGreatest(img, p1, p2, p3):
-	derivs1 = getDerivatives(img, p1[0], p1[0])
-	g1 = derivs1[0] * derivs1[0] + derivs1[1] * derivs1[1]
-
-	derivs2 = getDerivatives(img, p2[0], p2[0])
-	g2 = derivs2[0] * derivs2[0] + derivs2[1] * derivs2[1]
-
-	derivs3 = getDerivatives(img, p3[0], p3[0])
-	g3 = derivs3[0] * derivs3[0] + derivs3[1] * derivs3[1]
-	return (g1>g2 and g1>g3)
-
-def isStrong(img, i, j):
-	if(i<1 or j<1 or i>=img.shape[0] or j>=img.shape[1]):
-		return False
-
-	derivs = getDerivatives(img, i, j)
-	gx = derivs[0]
-	gy = derivs[1]
-	g = gx * gx + gy * gy
-
+def neighbours(gx, gy):
+	'''returns ((dx1, dy1), (dx2, dy2))'''
 	theta = atan2(gy, gx) * 8 # Multiply by eight because unit circle split into eights to get lines to check along
-	if((theta > (-pi) or theta < (pi)) or (theta > (7 * pi) or theta < (-7 * pi))): # or because can never be greater or less
-		# print("horizontal")
-		if(isGreatest(img, (i, j), (i-1, j), (i+1, j))):
-			return False
-		else:
-			return True
-	elif((theta > (pi) and theta < (3 * pi)) or (theta > (-7 * pi) and theta < (-5 * pi))): # diagonal up
-		# print("diagonal up")
-		if(isGreatest(img, (i, j), (i-1, j+1), (i+1, j-1))):
-			return False
-		else:
-			return True
+	print(theta)
+	if((theta > (pi) and theta < (3 * pi)) or (theta > (-7 * pi) and theta < (-5 * pi))): # diagonal up
+		return ((1, -1), (-1, 1))
 	elif((theta > (3 * pi) and theta < (5 * pi)) or (theta > (-5 * pi) and theta < (-3 * pi))): # vertical
-		# print("vertical")
-		if(isGreatest(img, (i, j), (i, j-1), (i, j+1))):
-			return False
-		else:
-			return True
+		return ((0, 1), (0, -1))
 	elif((theta > (5 * pi) and theta < (7 * pi)) or (theta > (-3 * pi) and theta < (-1 * pi))): # diagonal down
-		# print("diagonal down")
-		if(isGreatest(img, (i, j), (i-1, j-1), (i+1, j+1))):
-			return False
-		else:
-			return True
+		return ((-1, -1), (1, 1))
+	elif((theta > (-pi) or theta < (pi)) or (theta > (7 * pi) or theta < (-7 * pi))): # or because can never be greater or less
+		return ((-1, 0), (1, 0))
 	else:
-		# print("something must have been close?")
+		print("Something went wrong #1")
+		return ((0, 0), (0, 0))
+
+def canny1Pixel(img, row, col):
+	gx1, gy1 = getDerivatives(img, row, col)
+	g1 = gx1 * gx1 + gy1 * gy1
+
+	if(g1 < THRESHOLD * THRESHOLD):
+		return WHITE_PIXEL #Not even an edge
+
+	ds = neighbours(gx1, gy1)
+	dx2, dy2 = ds[0]
+	dx3, dy3 = ds[1]
+
+	gx2, gy2 = getDerivatives(img, row+dx2, col+dy2)
+	g2 = gx2 * gx2 + gy2 * gy2
+
+	gx3, gy3 = getDerivatives(img, row+dx3, col+dy3)
+	g3 = gx3 * gx3 + gy3 * gy3
+
+	if(g1 > g2 and g1 > g3):
+		return BLACK_PIXEL #stronger than neighbours
+	else:
+		return WHITE_PIXEL
+
+def canny1Image(image):
+	img = image.copy()
+	num_rows = img.shape[0] - 4
+	num_cols = img.shape[1] - 4
+	for i in range(2, num_rows):
+		for j in range(2, num_cols):
+			setPixel(img, i, j, canny1Pixel(image, i, j))
+	return img
+
+def canny2Pixel(img, row, col):
+	gx1, gy1 = getDerivatives(img, row, col)
+	g1 = gx1 * gx1 + gy1 * gy1
+
+	lt = THRESHOLD
+	ht = THRESHOLD * 2.5
+
+	if((g1 > lt * lt) and (g1 < ht * ht)): #within boundary
+		#check if neighbours are edges. If so, say its an edge, otherwise not
+		ds = neighbours(gx1, gy1)
+		dx2, dy2 = ds[0]
+		dx3, dy3 = ds[1]
+
+		if(img.item(row+dx2, col+dy2, 0) is 0 or img.item(row+dx3, col+dy3, 0) is 0):
+			return BLACK_PIXEL
+		else:
+			#return (0, 255, 0)
+			return False
+	else:
 		return False
 
-def neighbors(img, i, j, gx, gy):
-	for ii in range(-1, 2):
-		for jj in range(-1, 2):
-			if(img.item(i+ii, j+jj, 0) == 0):
-				print("found neighbor")
-				return True
-			else:
-				print(img.item(i+ii, j+jj, 0))
-				return False
-	# theta = atan2(gy, gx) * 8 # Multiply by eight because unit circle split into eights to get lines to check along
-	# if((theta > (-pi) or theta < (pi)) or (theta > (7 * pi) or theta < (-7 * pi))): # horizontal
-	# 	# print("horizontal")
-	# 	if(img.item(i-1, j, 0) == 0 or img.item(i+1, j, 0) == 0): #neighbors are black
-	# 		return True
-	# 	else:
-	# 		return False
-	# elif((theta > (pi) and theta < (3 * pi)) or (theta > (-7 * pi) and theta < (-5 * pi))): # diagonal up
-	# 	# print("diagonal up")
-	# 	if(img.item(i, j, 0) == 0 or img.item(i-1, j+1, 0) == 0):
-	# 		#neighbors are black
-	# 		return True
-	# 	else:
-	# 		return False
-	# elif((theta > (3 * pi) and theta < (5 * pi)) or (theta > (-5 * pi) and theta < (-3 * pi))): # vertical
-	# 	# print("vertical")
-	# 	if(img.item(i, j-1, 0) == 0 or img.item(i, j+1, 0) == 0): 
-	# 		#neighbors are black
-	# 		return True
-	# 	else:
-	# 		return False
-	# elif((theta > (5 * pi) and theta < (7 * pi)) or (theta > (-3 * pi) and theta < (-1 * pi))): # diagonal down
-	# 	# print("diagonal down")
-	# 	if(img.item(i-1, j-1, 0) == 0 or img.item(i+1, j+1, 0) == 0):
-	# 		#neighbors are black
-	# 		return True
-	# 	else:
-	# 		return False
+def canny2Image(image):
+	img = image.copy()
+	num_rows = img.shape[0] - 4
+	num_cols = img.shape[1] - 4
+	for i in range(2, num_rows):
+		for j in range(2, num_cols):
+			r = canny2Pixel(image, i, j)
+			if(r):
+				setPixel(img, i, j, r)
+	return img
+
+def drawLine(img, row, col):
+	'''return new image with line'''
+	gx, gy = getDerivatives(img, row, col)
+	# if(gx is not 0):
+	# 	slope = int(gy/gx)
 	# else:
-	# 	# print("something must have been close?")
-	# 	return True
-
-
-def isEdge2(img, i, j, lower_t, upper_t):
-	if(i<1 or j<1 or i>=img.shape[0] or j>=img.shape[1]):
-		return False
-
-	derivs = getDerivatives(img, i, j)
-	gx = derivs[0]
-	gy = derivs[1]
-	g = gx * gx + gy * gy
-	print(g, lower_t * lower_t, upper_t * upper_t)
-
-	if(g > upper_t * upper_t):
-		return False
-	elif(g < lower_t * lower_t):
-		return True
-	else:
-		print("mid threshold")
-		#contested edge
-		if(neighbors(img, i, j, gx, gy)):
-			print("Filled in a pixel during canny2")
-			return True
-		else:
-			print("Apparently not neighboring anything")
-			return False
-
-
-def canny1(image):
-	img = image.copy()
-	num_rows = img.shape[0] - 4
-	num_cols = img.shape[1] - 4
-	#Pass 1 of canny
-	for i in range(2, num_rows):
-		for j in range(2, num_cols):
-			if(isEdge(image, i, j, MID_THRESHOLD-(MID_THRESHOLD/2), MID_THRESHOLD+DT)): #image is not changed, img is changed
-				setPixel(img, i, j, (255, 255, 255)) 
-	return img
-def canny2(image):
-	img = image.copy()
-	num_rows = img.shape[0] - 4
-	num_cols = img.shape[1] - 4
-	for i in range(2, num_rows):
-		for j in range(2, num_cols):
-			if(img.item(i, j, 0) == 0): # black, and therefore edge
-				if(isEdge2(image, i, j, MID_THRESHOLD-(MID_THRESHOLD/2), MID_THRESHOLD)): 
-					setPixel(img, i, j, (0, 0, 0)) # strong edges, fill with black
+	# 	slope = 0
+	slope = 1
+	num_rows = img.shape[0]
+	for i in range(0, row):
+		setPixel(img, i, (row-i) * -slope, RED_PIXEL)
+	for i in range(row, num_rows):
+		setPixel(img, i, -slope * i, RED_PIXEL)
 	return img
 
-def keystroke():
-	#Escape key to exit
-	k = cv2.waitKey(0)
-	if k == 27:         # wait for ESC key to exit
-	    cv2.destroyAllWindows()
-	    exit()
-	elif k == ord('n'):
-		cv2.destroyAllWindows()
-		cv2.imshow('color_image',image)
-		print('showing normal image')
-	elif k == ord('g'):
-		cv2.destroyAllWindows()
-		cv2.imshow('gray_image',gray_image)
-		print('showing gray image')
-	elif k == ord('b'):
-		cv2.destroyAllWindows()
-		cv2.imshow('blur_image',blur_image)
-		print('showing blur image')
-	elif k == ord('s'):
-		cv2.destroyAllWindows()
-		cv2.imshow('sobel_image',sobel_image)
-		print('showing sobel image')
-	elif k == ord('c'):
-		cv2.destroyAllWindows()
-		cv2.imshow('canny_image',canny_image)
-		print('showing canny image')
 
-	print("recursing")
-	keystroke()
-
-location = sys.argv[1]
-
-MID_THRESHOLD = int(sys.argv[2])
-DT = 25
-
-#if I want to read from URL, do that here. Until them, I treat it as a local url 
-image = cv2.imread(location)
+image = cv2.imread(LOCATION)
 cv2.imshow('color_image',image)
 
 gray_image = grayImage(image)
-cv2.imshow('gray_image',gray_image)
-if(location == "dog.jpg"):
+cv2.imshow('gray_image', gray_image)
+if(LOCATION == "dog.jpg"):
 	cv2.imwrite("gray_dog.jpg", gray_image)
 
 blur_image = blurImage(gray_image)
-cv2.imshow('blur_image',blur_image)
-if(location == "dog.jpg"):
+cv2.imshow('blur_image', blur_image)
+if(LOCATION == "dog.jpg"):
 	cv2.imwrite("blur_dog.jpg", blur_image)
 
 sobel_image = sobelImage(gray_image)
-cv2.imshow('sobel_image',sobel_image)
-if(location == "dog.jpg"):
+cv2.imshow('sobel_image', sobel_image)
+if(LOCATION == "dog.jpg"):
 	cv2.imwrite("sobel_dog.jpg", sobel_image)
 
-canny_image1 = canny1(sobel_image)
-cv2.imshow('canny_image1', canny_image1)
-if(location == "dog.jpg"):
-	cv2.imwrite("canny1_dog.jpg", canny_image1)
+canny1_image = canny1Image(gray_image)
+cv2.imshow('canny1_image', canny1_image)
+if(LOCATION == "dog.jpg"):
+	cv2.imwrite("canny1_dog.jpg", canny1_image)
 
-canny_image2 = canny2(canny_image1)
-cv2.imshow('canny_image2', canny_image2)
-if(location == "dog.jpg"):
-	cv2.imwrite("canny2_dog.jpg", canny_image2)
+canny2_image = canny2Image(canny1_image)
+cv2.imshow('canny2_image', canny2_image)
+if(LOCATION == "dog.jpg"):
+	cv2.imwrite("canny2_dog.jpg", canny2_image)
+
+#cv2.imshow('line_image', drawLine(gray_image, 20, 20))
+
 
 keystroke()
-
-# Print out with
-# ORIGINAL IMAGE
-# SOBEL EDGES ONLY
-# EDGES AFTER CANNY IS APPLIED
-# A. AFTER EDGE THINNING
-# B. AFTER WEEK EDGES ARE INCORPORATED
