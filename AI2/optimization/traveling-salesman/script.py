1675711a5511c794f5eaa1a49b261a428a68fb39
Rushi Shah <2016rshah@gmail.com>
2016-04-27 21:58:04 -0400

    Combined methods for a new personal best

--- AI2/optimization/traveling-salesman/script.py
+++ AI2/optimization/traveling-salesman/script.py
@@ -153,163 +153,6 @@ def loopThroughAllPossibilities(fs):
 			break
 	return currSwap
 
-# def calcArea(p1,p2,p3):
-# 	#dividing by 2 won't change sign, so don't bother
-# 	px, py = p1.latlon()
-# 	qx, qy = p2.latlon()
-# 	rx, ry = p3.latlon()
-# 	x = (px*qy + qx*ry + rx*py)
-# 	y = (px*ry  + qx*py + rx*qy)
-# 	return (x-y)
-
-# def sameSign(x, y):
-# 	return ((x<0) == (y<0))
-
-# def isCrossed(fs, p1, p2):
-# 	p0 = fs[fs.index(p1) - 1]
-# 	p3i = fs.index(p2) + 1
-# 	if(p3i < len(fs)): 
-# 		p3 = fs[p3i]
-# 	else:
-# 		p3 = fs[0]
-# 	a1 = calcArea(p0, p1, p2)
-# 	a2 = calcArea(p0, p1, p3)
-# 	a3 = calcArea(p2, p3, p0)
-# 	a4 = calcArea(p2, p3, p1)
-# 	if(sameSign(a1, a2) or sameSign(a3, a4)): #this OR is very important. It can't be an AND lol
-# 		#Both sets of both areas have the same sign
-# 		#which means the lines are NOT crossed
-# 		return False
-# 	else:
-# 		return True
-
-# def findAndRev(fs):
-# 	for i in range(0, len(fs)):
-# 		for j in range(i, len(fs)-1):
-# 			if(not i == j):
-# 				if isCrossed(fs, fs[i], fs[j]):
-# 					print("swapped", i, j)
-# 					# fs[i], fs[j] = fs[j], fs[i]
-# 					fs[i:(j+1)] = reversed(fs[i:(j+1)])
-# 					return fs #sometimes, undoing one cross creates another cross, and it'll just uncross and recross
-# 	return False
-
-# def uncrossEverything(fs):
-# 	foundSwap = True
-# 	bestPath = list(fs)
-# 	while foundSwap:
-# 		newPath = findAndRev(fs) #might create a cross which is worse
-# 		if(newPath is False):
-# 			break
-# 		else:
-# 			bestPath = newPath
-# 			print(distanceTraveled(bestPath))
-# 			plotPath(bestPath)
-# 			plt.pause(0.05)
-# 	return bestPath
-
-# def combineMethods(fs):
-# 	distanceImprovedSinceLastSwap = 0
-# 	print(len(fs))
-# 	currMin = distanceTraveled(fs)
-# 	print(currMin)
-# 	currSwap = list(fs)
-# 	swapsSinceImprovement = 0
-# 	for i in range(0, 10000):
-# 		bestswap = findBestSwap(currSwap)
-# 		d = distanceTraveled(bestswap)
-# 		if(d < currMin and abs(d - currMin) > 1):
-# 			print("Found new best distance: ", d)
-# 			if(distanceImprovedSinceLastSwap > 250):
-# 				distanceImprovedSinceLastSwap = 0
-# 				plotPath(currSwap)
-# 				plt.pause(0.05)
-# 			else:
-# 				distanceImprovedSinceLastSwap += (currMin - d)
-# 				print("not a great improvement", distanceImprovedSinceLastSwap)
-# 			currSwap = list(bestswap)
-# 			currMin = d
-# 			swapsSinceImprovement = 0
-# 			#now uncross everything
-# 			currSwap = uncrossEverything(currSwap)
-# 			currMin = distanceTraveled(currSwap)
-# 		else:
-# 			swapsSinceImprovement += 1
-# 		if(swapsSinceImprovement > THRESHOLD_TWO):
-# 			print("Haven't improved in about ", swapsSinceImprovement, " swaps, so exiting at")
-# 			break
-# 	return currSwap
-
-# def incrementalUncross(fs, pd, i, j):
-# 	''' fs list of locations
-# 		pd previously calculated distance for fs
-# 		i first index
-# 		j second index
-# 		return tuple with path and distance 
-# 	'''
-# 	#this part is wrong because assigning a and b incorrectly
-# 	# b = i
-# 	# a = b-1
-# 	# c = j
-# 	# if j+1 < len(fs):
-# 	# 	d = j+1
-# 	# else:
-# 	# 	d = 0
-# 	# incAdd = fs[a].distanceTo(fs[c]) + fs[b].distanceTo(fs[d]) 
-# 	# incSub = fs[a].distanceTo(fs[b]) + fs[c].distanceTo(fs[d])
-
-# 	b = i
-# 	a = b - 1
-# 	d = j
-# 	c = j - 1
-
-
-# 	incAdd = fs[a].distanceTo(fs[d]) + fs[c].distanceTo(fs[b])
-# 	incSub = fs[a].distanceTo(fs[b]) + fs[c].distanceTo(fs[d])
-
-# 	incDiff = incAdd - incSub
-# 	if incDiff > -.001:
-# 		return (fs, pd) #no need to change anything
-# 	else:
-# 		print("incDiff", incDiff)
-# 		#made an improvement
-# 		#reverse the necessary part
-# 		print(fs)
-# 		fs[(i):(j+1)] = reversed(fs[(i):(j+1)])
-# 		print(fs)
-# 		#calculate new distance
-# 		newd = pd + incDiff
-# 		return (fs, newd)
-# 		#return the new path and new distance
-
-# def untangle(fs, pd=-1):
-# 	if pd is -1:
-# 		currDistance = distanceTraveled(fs)
-# 	else:
-# 		currDistance = pd
-# 	currPath = list(fs)
-# 	shouldLoop = True
-# 	while(shouldLoop):
-# 	# for _ in range(3):
-# 		shouldLoop = False
-# 		for i in range(1, len(fs)-1):
-# 			for j in range(i, len(fs)):
-# 				pp, pd = currPath, float(currDistance)
-# 				currPath, currDistance = incrementalUncross(pp, pd, i, j)
-# 				print("check incremental distance traveled", distanceTraveled(currPath), currDistance)
-# 				#improved
-# 				if(currDistance < pd):
-# 					# print("caught new best", currDistance, pd)
-# 					print(currDistance)
-# 					plotPath(currPath)
-# 					plt.pause(0.05)
-# 					shouldLoop = True
-# 			# else:
-# 				# print("did not improve", currDistance, pd)
-# 	print("calculated distance", currPath)
-# 	print("actual distance", distanceTraveled(currPath))
-# 	return currPath
-
 def untangle(fs):
 	currDistance = distanceTraveled(fs)
 	currPath = list(fs)
@@ -335,21 +178,38 @@ def untangle(fs):
 					shouldLoop = True
 		plotPath(currPath)
 		plt.pause(0.05)
-	return currPath
+	return currPath, currDistance
+
+def combineMethods(fs):
+	currMin = distanceTraveled(fs)
+	print(currMin)
+	currSwap = list(fs)
+	swapsSinceImprovement = 0
+	# for i in range(0, 10):
+	bestswap = findBestSwap(currSwap)
+	d = distanceTraveled(bestswap)
+	if(d < currMin):
+		bestswap, d = untangle(bestswap)
+		print("Found new best distance: ", d, distanceTraveled(bestswap))
+		currSwap = list(bestswap)
+		currMin = d
+		swapsSinceImprovement = 0
+		plotPath(currSwap)
+		plt.pause(0.05)
+	else:
+		swapsSinceImprovement += 1
+		# print("iterating at", currMin)
+		# print("iterating")
+		# if(swapsSinceImprovement > THRESHOLD_TWO):
+		# 	print("Haven't improved in about ", swapsSinceImprovement, " swaps, so exiting at")
+		# 	break
+	return currSwap
 
 def main():
 	plt.ion()
 	fs = getLocations("input_large.txt")
 
-	# bestPath = uncrossEverything(fs)
-
-	# bestPath = loopThroughAllPossibilities(fs)
-	
-	# bestPath = combineMethods(fs)
-	# plotPath(fs)
-	# bestPath = fs
-	# bestPath = untangle(fs)
-	bestPath = untangle(fs)
+	bestPath = combineMethods(fs)
 
 	bestPath = startFrom1(bestPath)
 	
