938d45aa66d9873acf494c6b291704a6bfbeeb7e
Rushi Shah <2016rshah@gmail.com>
2016-05-19 14:26:31 -0400

    Finished initial part of lab

--- AI2/neural-networks/script.py
+++ AI2/neural-networks/script.py
@@ -8,64 +8,66 @@ def f(w, b, x):
 def g(w,b,x,y):
 	return 1 / (1 + (e ** (-w * (x+y) + b)))
 
-# NOT
-# 0 -> 1
-# 1 -> 0
-def notHillClimbing():
-	for w in range(100,0,-1):
-		for b in range(100,0,-1):
-			y0 = f(w,b,0)
-			y1 = f(w,b,1)
-			if(y0 < .1 and y1 > .9):
-				print(w,b)
-#I'm not sure if this is okay
-def nnNot(x):
-	if(f(6, 3, x) < .1):
-		return 1
-	else:
-		return 0
-#6 and 3 look pretty good
+# not -> singleValueHillClimbing([0,1],[1,0])
+def singleValueHillClimbing(xs, ys):
+	bestW = 0
+	bestB = 0
+	bestError = 100000000
+	w = 100
+	b = 100
+	while w > -100:
+		while b > -100:
+			# print(w,b)
+			currError = 0
+			for i in range(len(xs)):
+				currError += (ys[i] - f(w, b, xs[i])) ** 2
+			# print(w,b,currError)
+			if(currError < bestError):
+				bestError = currError
+				bestW = w
+				bestB = b
+				print(bestW, bestB, currError)
+			b -= .1
+		b = 100
+		w -= .1
 
-# AND
-# 0 -> 0 -> 0
-# 0 -> 1 -> 0
-# 1 -> 0 -> 0
-# 1 -> 1 -> 1
-def andHillClimbing():
-	for w in range(100,0,-1):
-		for b in range(100,0,-1):
-			y00 = g(w,b,0,0)
-			y01 = g(w,b,0,1)
-			y10 = g(w,b,1,0)
-			y11 = g(w,b,1,1)
-			if (y00 < .1 and y01 < .1 and y10 < .1 and y11 > .9):
-				print(w,b)
-#6 and 9 work okay
+# # AND
+# # 0 -> 0 -> 0
+# # 0 -> 1 -> 0
+# # 1 -> 0 -> 0
+# # 1 -> 1 -> 1
+# [0,0,1,1]
+# [0,1,0,1]
+# [0,0,0,1]
 
-def nnAnd(x,y):
-	if(g(6,9,x,y) < .5):
-		return 0
-	else:
-		return 1
+# # OR
+# # 0 -> 0 -> 0
+# # 0 -> 1 -> 1
+# # 1 -> 0 -> 1
+# # 1 -> 1 -> 1
+# [0,0,1,1]
+# [0,1,0,1]
+# [0,1,1,1]
 
-# OR
-# 0 -> 0 -> 0
-# 0 -> 1 -> 1
-# 1 -> 0 -> 1
-# 1 -> 1 -> 1
-def orHillClimbing():
-	for w in range(100,0,-1):
-		for b in range(100,0,-1):
-			y00 = g(w,b,0,0)
-			y01 = g(w,b,0,1)
-			y10 = g(w,b,1,0)
-			y11 = g(w,b,1,1)
-			if (y00 < .1 and y01 > .9 and y10 > .9 and y11 > .9):
-				print(w,b)
-#6 and 3 work okay
-
-def nnOr(x,y):
-	if(g(6,3,x,y) < .5):
-		return 0
-	else:
-		return 1
+# doubleValueHillClimbing([0,1],[1,0])
+def doubleValueHillClimbing(xs, ys, zs):
+	bestW = 0
+	bestB = 0
+	bestError = 100000000
+	w = 100
+	b = 100
+	while w > -100:
+		while b > -100:
+			# print(w,b)
+			currError = 0
+			for i in range(len(xs)):
+				currError += (zs[i] - g(w, b, xs[i], ys[i])) ** 2
+			# print(w,b,currError)
+			if(currError < bestError):
+				bestError = currError
+				bestW = w
+				bestB = b
+				print(bestW, bestB, currError)
+			b -= .1
+		b = 100
+		w -= .1
